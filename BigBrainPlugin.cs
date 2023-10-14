4d9701cda3b89d712bbfc66e582ecf8aec90bb08
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-10-14 12:52:50 -0700

    Update for 3.7.1 / 26535

--- BigBrainPlugin.cs
+++ BigBrainPlugin.cs
@@ -5,8 +5,8 @@ using System;
 
 namespace DrakiaXYZ.BigBrain
 {
-    [BepInPlugin("xyz.drakia.bigbrain", "DrakiaXYZ-BigBrain", "0.3.0.0")]
-    [BepInDependency("com.spt-aki.core", "3.7.0")]
+    [BepInPlugin("xyz.drakia.bigbrain", "DrakiaXYZ-BigBrain", "0.3.1.0")]
+    [BepInDependency("com.spt-aki.core", "3.7.1")]
     internal class BigBrainPlugin : BaseUnityPlugin
     {
         private void Awake()
